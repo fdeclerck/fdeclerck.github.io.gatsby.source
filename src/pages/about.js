efc70414d670b168ccda184c1b3bb7dc4ffd3ec8
Franck Genius de Clerck <franck.de-clerck@pole-emploi.fr>
2019-04-26 17:12:56 +0200

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText="fdeclerck.github.io" />
+    <Header headerText="Personal GitHub website" />  
+    <p>Such wow. Very React.</p>
+  </div>
+)
